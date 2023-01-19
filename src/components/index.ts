5fb08658c04f0a2e446355ad58c6cc361296ec9a
tuan.le2 <tuan.le2@sotatek.com>
2023-01-19 16:44:19 +0700

    feat: update websocket hook

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as PublicRoute } from './Route/PublicRoute';
 export { default as PrivateRoute } from './Route/PrivateRoute';
 export { default as AppButton } from './AppButton';
+export { default as AppInput } from './AppInput';
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { AppButton, AppInput } from 'src/components';
+import { useWebSocket } from 'src/hooks';
 import 'src/styles/pages/HomePage.scss';
 
 const HomePage = () => {
-  return <>Home Page</>;
+  const [input, setInput] = useState<string>('');
+  const [webSocketURL, setWebSocketURL] = useState<string>('');
+  const [messages, setMessages] = useState<any[]>([]);
+  const { connectionStatus, latestMessage } = useWebSocket(webSocketURL);
+
+  useEffect(() => {
+    if (latestMessage) {
+      setMessages((prevState) => [...prevState, latestMessage]);
+    }
+  }, [latestMessage]);
+
+  const onChangeWebSocketURL = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  return (
+    <div>
+      Home Page
+      <p>
+        WebSocket URL Example:
+        wss://bstream.binance.com:9443/stream?streams=abnormaltradingnotices
+      </p>
+      <AppInput value={input} onChange={onChangeWebSocketURL} />
+      <AppButton variant="main" onClick={() => setWebSocketURL(input)}>
+        Run
+      </AppButton>
+      <p>Web Socket: {webSocketURL}</p>
+      <p>Connection status: {connectionStatus}</p>
+      <p>Last Message: {latestMessage}</p>
+      <p>All Messages:</p>
+      {messages.map((message, index) => (
+        <>
+          <p key={index}>{message}</p>
+          <br />
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default HomePage;
