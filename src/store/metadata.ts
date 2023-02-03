b8445df317fdab5e4012a2a91ad24c336c01afb0
tuan.le2 <tuan.le2@sotatek.com>
2023-02-03 09:13:37 +0700

    feat: init metadata with tickers

--- src/store/metadata.ts
+++ src/store/metadata.ts
@@ -1,23 +1,54 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-// import rf from 'src/services/RequestFactory';
+import rf from 'src/services/RequestFactory';
 
-export type MetadataState = {};
+export interface ITicker {
+  symbol: string;
+  priceChange: string;
+  priceChangePercent: string;
+  lastPrice: string;
+  lastPriceChange: string;
+  highPrice: string;
+  lowPrice: string;
+  volume: string;
+  quoteVolume: string;
+  indexPrice: string;
+  oraclePrice: string;
+  fundingRate: string;
+}
 
-const initialState: MetadataState = {};
+export type MetadataState = {
+  tickers: ITicker[];
+};
+
+const initialState: MetadataState = {
+  tickers: [],
+};
+
+export const getMetadataTickers = createAsyncThunk(
+  'metadata/getTickers',
+  async (_params, thunkApi) => {
+    const res = await rf.getRequest('TickerRequest').getTicker24h();
+    thunkApi.dispatch(setTickers(res));
+  },
+);
 
 export const initMetadata = createAsyncThunk(
   'metadata/init',
   async (_params, thunkApi) => {
-    //
+    thunkApi.dispatch(getMetadataTickers());
   },
 );
 
 const metadataSlice = createSlice({
   name: 'metadata',
   initialState,
-  reducers: {},
+  reducers: {
+    setTickers: (state, action) => {
+      state.tickers = action.payload;
+    },
+  },
 });
 
-export const {} = metadataSlice.actions;
+export const { setTickers } = metadataSlice.actions;
 
 export default metadataSlice.reducer;
