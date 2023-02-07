e950564ab3276d8f16b4e6fb171516ffdf471319
tuan.le2 <tuan.le2@sotatek.com>
2023-02-07 14:43:53 +0700

    feat: add mock api for ticker symbols

---
+++ src/services/MetadataRequest.ts
@@ -0,0 +1,13 @@
+import BaseRequest from './BaseRequest';
+
+export default class MetadataRequest extends BaseRequest {
+  getTicker24h() {
+    const url = '/ticker/24h';
+    return this.get(url);
+  }
+
+  getSymbols() {
+    const url = '/ticker/symbols';
+    return this.get(url);
+  }
+}
--- src/services/TickerRequest.ts
+++
@@ -1,8 +0,0 @@
-import BaseRequest from './BaseRequest';
-
-export default class TickerRequest extends BaseRequest {
-  getTicker24h() {
-    const url = '/ticker/24h';
-    return this.get(url);
-  }
-}
